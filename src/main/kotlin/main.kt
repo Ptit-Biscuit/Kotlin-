9a213dd104633cb9f2faa19972798d469a658799
Vincent Brebion <vbrebion@sodius.com>
2020-07-16 16:53:02 +0200

    Matrix for rooms layout (IWP)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,33 +4,34 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 import org.openrndr.draw.loadFont
 import org.openrndr.math.Vector2
+import kotlin.math.floor
 import kotlin.random.Random
 
 // ---------- SEED ---------- //
 fun generateSeed() = Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE)
 
 // ---------- ROOM ---------- //
-const val ROOM_WIDTH = 50
+const val ROOM_WIDTH = 50.0
 const val ROOM_OPENING_WIDTH = ROOM_WIDTH / 3.0
 
-val moveDown = Vector2(.0, .0 + ROOM_WIDTH)
-val moveRight = Vector2(.0 + ROOM_WIDTH, .0)
-val moveDownRight = Vector2(.0 + ROOM_WIDTH, .0 + ROOM_WIDTH)
-val horizontalOpeningStart = Vector2(.0 + ROOM_OPENING_WIDTH, .0)
-val horizontalOpeningEnd = Vector2(.0 + ROOM_WIDTH - ROOM_OPENING_WIDTH, .0)
-val verticalOpeningStart = Vector2(.0, .0 + ROOM_OPENING_WIDTH)
-val verticalOpeningEnd = Vector2(.0, .0 + ROOM_WIDTH - ROOM_OPENING_WIDTH)
+val moveDown = Vector2(.0, ROOM_WIDTH)
+val moveRight = Vector2(ROOM_WIDTH, .0)
+val moveDownRight = Vector2(ROOM_WIDTH, ROOM_WIDTH)
+val horizontalOpeningStart = Vector2(ROOM_OPENING_WIDTH, .0)
+val horizontalOpeningEnd = Vector2(ROOM_WIDTH - ROOM_OPENING_WIDTH, .0)
+val verticalOpeningStart = Vector2(.0, ROOM_OPENING_WIDTH)
+val verticalOpeningEnd = Vector2(.0, ROOM_WIDTH - ROOM_OPENING_WIDTH)
 
 data class Room(val pos: Vector2, val openings: MutableCollection<Direction>)
 
-fun generateRooms(rnd: Random, width: Int, height: Int): MutableList<Room> {
+fun generateRooms(rnd: Random, grid: List<MutableList<Boolean>>, num: Int): MutableList<Room> {
     val rooms = mutableListOf<Room>()
-    val firstRoomPos = Vector2((width / 2.0) - (ROOM_WIDTH / 2), (height / 2.0) - (ROOM_WIDTH / 2))
+    val firstRoomPos = Vector2(floor(grid.size / 2.0), floor(grid[0].size / 2.0))
 
     // first room
     rooms.add(Room(firstRoomPos, mutableSetOf()))
 
-    (1..5).forEach {
+    (1 until num).forEach {
         rooms.add(generateRoom(rnd, rooms[it - 1]))
     }
 
@@ -55,38 +56,50 @@ fun generateRoom(rnd: Random, previousRoom: Room): Room {
     return Room(pos, mutableSetOf(oppositeDirection(side)))
 }
 
+// ---------- GRID ---------- //
+fun generateGrid(width: Int, height: Int) =
+    MutableList(width / ROOM_WIDTH.toInt() - 1) { MutableList(height / ROOM_WIDTH.toInt() - 2) { false } }
+
+fun toWorldPos(pos: Vector2) = Vector2(pos.x * ROOM_WIDTH + ROOM_WIDTH, pos.y * ROOM_WIDTH + (ROOM_WIDTH * 2))
+
 // ---------- DRAW UTILS ---------- //
 fun drawRoom(drawer: Drawer, room: Room) {
+    // convert grid position to world position
+    val worldPos = toWorldPos(room.pos)
+
+    println(room.pos)
+    println(worldPos)
+
     // NORTH side
     if (room.openings.contains(Direction.NORTH)) {
-        drawer.lineSegment(room.pos, room.pos + horizontalOpeningStart)
-        drawer.lineSegment(room.pos + horizontalOpeningEnd, room.pos + moveRight)
+        drawer.lineSegment(worldPos, worldPos + horizontalOpeningStart)
+        drawer.lineSegment(worldPos + horizontalOpeningEnd, worldPos + moveRight)
     } else {
-        drawer.lineSegment(room.pos, room.pos + moveRight)
+        drawer.lineSegment(worldPos, worldPos + moveRight)
     }
 
     // EAST side
     if (room.openings.contains(Direction.EAST)) {
-        drawer.lineSegment(room.pos + moveRight, room.pos + moveRight + verticalOpeningStart)
-        drawer.lineSegment(room.pos + moveRight + verticalOpeningEnd, room.pos + moveDownRight)
+        drawer.lineSegment(worldPos + moveRight, worldPos + moveRight + verticalOpeningStart)
+        drawer.lineSegment(worldPos + moveRight + verticalOpeningEnd, worldPos + moveDownRight)
     } else {
-        drawer.lineSegment(room.pos + moveRight, room.pos + moveDownRight)
+        drawer.lineSegment(worldPos + moveRight, worldPos + moveDownRight)
     }
 
     // SOUTH side
     if (room.openings.contains(Direction.SOUTH)) {
-        drawer.lineSegment(room.pos + moveDown, room.pos + moveDown + horizontalOpeningStart)
-        drawer.lineSegment(room.pos + moveDown + horizontalOpeningEnd, room.pos + moveDownRight)
+        drawer.lineSegment(worldPos + moveDown, worldPos + moveDown + horizontalOpeningStart)
+        drawer.lineSegment(worldPos + moveDown + horizontalOpeningEnd, worldPos + moveDownRight)
     } else {
-        drawer.lineSegment(room.pos + moveDown, room.pos + moveDownRight)
+        drawer.lineSegment(worldPos + moveDown, worldPos + moveDownRight)
     }
 
     // WEST side
     if (room.openings.contains(Direction.WEST)) {
-        drawer.lineSegment(room.pos, room.pos + verticalOpeningStart)
-        drawer.lineSegment(room.pos + verticalOpeningEnd, room.pos + moveDown)
+        drawer.lineSegment(worldPos, worldPos + verticalOpeningStart)
+        drawer.lineSegment(worldPos + verticalOpeningEnd, worldPos + moveDown)
     } else {
-        drawer.lineSegment(room.pos, room.pos + moveDown)
+        drawer.lineSegment(worldPos, worldPos + moveDown)
     }
 }
 
@@ -101,6 +114,7 @@ fun oppositeDirection(direction: Direction) = Direction.values()[(direction.ordi
 fun main() = application {
     var seed = generateSeed()
     val rnd = Random(seed)
+    val numberOfRooms = 2
 
     configure {
         width = 900
@@ -109,26 +123,43 @@ fun main() = application {
 
     program {
         val font = loadFont("src/main/resources/VCR_OSD_MONO_1.001.ttf", 14.0)
-        var rooms = generateRooms(rnd, width, height)
+        val grid = generateGrid(width, height)
+        var rooms = generateRooms(rnd, grid, numberOfRooms)
 
         // listeners
         keyboard.keyDown.listen {
             if (it.key == KEY_ENTER) {
                 seed = generateSeed()
                 rooms.clear()
-                rooms = generateRooms(rnd, width, height)
+                rooms = generateRooms(rnd, grid, numberOfRooms)
             }
         }
 
         extend {
             drawer.clear(ColorRGBa.BLACK)
             drawer.fontMap = font
-            drawer.fill = ColorRGBa.WHITE
             drawer.stroke = ColorRGBa.WHITE
             drawer.strokeWeight = 2.0
 
             // display useful data
             drawer.text("{Seed: ${seed.toUInt().toString(36)}}", 20.0, 30.0)
+
+            // draw grid points
+            grid.forEachIndexed { indexX, x ->
+                x.forEachIndexed { indexY, roomPresent ->
+                    // green if no room else red
+                    drawer.stroke = if (roomPresent) ColorRGBa.RED else ColorRGBa.GREEN
+
+                    drawer.circle(
+                        ROOM_WIDTH + indexX.toDouble() * ROOM_WIDTH,
+                        (ROOM_WIDTH * 2) + indexY.toDouble() * ROOM_WIDTH,
+                        2.0
+                    )
+                }
+            }
+
+            // reset stroke
+            drawer.stroke = ColorRGBa.WHITE
 
             // draw rooms
             rooms.forEach { r ->
